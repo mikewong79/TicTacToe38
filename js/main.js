cf43cff89b6ec69e65c308a59aa40e6131c077c1
Mike Wong <mikewong79@gmail.com>
2016-10-03 11:42:23 -0700

    added basic click handler to each cell

--- js/main.js
+++ js/main.js
@@ -1,1 +1,11 @@
 console.log("main.js loaded")
+
+var cells = document.getElementsByClassName('cell');
+console.log(cells);
+
+for(var i = 0; i < cells.length; i++) {
+  cells[i].addEventListener('click', function() {
+    alert("you clicked on a cell");
+    console.log(this);
+  });
+}
